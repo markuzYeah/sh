9ad3d2db4b122805ed4e140a070e214f5ed47da0
markuzYeah <markuz.yeah@gmail.com>
2013-06-20 16:51:10 -0700

    fixing the mac installer

--- installer.bash
+++ installer.bash
@@ -11,8 +11,8 @@ gitBranch='master'
 export GIT_PATH="$gitAcc/$gitRepo/$gitBranch"
 
 if uname -v | grep -qi 'ubuntu'; then
-  setup="https://raw.github.com/$GIT_PATH/sh/setup.ub.bash"
-  deps="https://raw.github.com/$GIT_PATH/sh/deps.bash"
+  setup="https://raw.github.com/$GIT_PATH/setup.ub.bash"
+  deps="https://raw.github.com/$GIT_PATH/deps.bash"
 
   python -c "import urllib2; print urllib2.urlopen('$setup').read()"|sh
   #python -c "import urllib2; print urllib2.urlopen('$deps').read()"|sh
