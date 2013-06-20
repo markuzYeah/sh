344966f2ce2017ec631641e1bca702340319f418
markuzYeah <markuz.yeah@gmail.com>
2013-06-20 16:51:49 -0700

    fixing the mac installer

--- installer.bash
+++ installer.bash
@@ -21,8 +21,8 @@ if uname -v | grep -qi 'ubuntu'; then
   
   #python -c "import urllib2; print urllib2.urlopen('$urlLinux').read()"|sh
 elif uname -v | grep -qi 'darwin'; then
-  setup="https://raw.github.com/$GIT_PATH/sh/setup.mac.bash"
-  #deps="https://raw.github.com/$GIT_PATH/sh/deps.bash"
+  setup="https://raw.github.com/$GIT_PATH/setup.mac.bash"
+  #deps="https://raw.github.com/$GIT_PATH/deps.bash"
 
   python -c "import urllib2; print urllib2.urlopen('$setup').read()"|sh
   #python -c "import urllib2; print urllib2.urlopen('$deps').read()"|sh
